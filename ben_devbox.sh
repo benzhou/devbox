eedba454224ee2708ea54883344c5d22155798e4
Ben Zhou <amailtoaben@gmail.com>
2014-10-08 17:36:15 -0400

    added tmux conf, symlinks it to user home, and installed some other dev code

--- ben_devbox.sh
+++ ben_devbox.sh
@@ -2,19 +2,32 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew tap caskroom/cask
 brew install brew-cask
 brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup
-brew cask install java
+
 brew cask install google-chrome
 brew cask install alfred
-brew cask install vagrant
-brew cask install webstorm
-brew cask install slack
-brew cask install virtualbox
-brew cask install atom
 brew cask install skype
 brew cask install the-unarchiver
 
+#Dev installs
+brew cask install java
+brew cask install vagrant
+brew cask install virtualbox
+
+brew cask install slack
+
+#Intellij
+brew cask install intellij-idea-ce
+brew cask install webstorm
+brew cask install atom
+
 brew install wget
+
+#Dev install
 brew install git
+brew install nodejs
+brew install pandoc
+
+npm install -g markmon
 
 #Symlimk for bash completion
 ln -s /usr/local/Cellar/git/2.1.2/etc/bash_completion.d/git-completion.bash ~/.git-completion.bash
@@ -23,3 +36,4 @@ ln -s /usr/local/Cellar/git/2.1.2/etc/bash_completion.d/git-prompt.sh ~/.git-pro
 ln -s ~/dev/devbox/.bash_profile ~/.bash_profile
 ln -s ~/dev/devbox/.bashrc ~/.bashrc
 ln -s ~/dev/devbox/.gitconfig ~/.gitconfig
+ln -s ~/dev/devbox/tmux.conf ~/.tmux.conf
